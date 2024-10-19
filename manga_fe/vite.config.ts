df2173b86190ca6f63926114a5ccffe10ec48179
AndrewGPFranco <andrewgomes1328@gmail.com>
2024-10-18 23:38:34 -0300

    feat: usando docker para subir o servidor

--- manga_fe/vite.config.ts
+++ manga_fe/vite.config.ts
@@ -13,5 +13,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 })
