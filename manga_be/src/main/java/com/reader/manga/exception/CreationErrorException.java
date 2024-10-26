8634154989dd0ec66e5707c0b2bc7295f57d3513
AndrewGPFranco <andrewgomes1328@gmail.com>
2024-10-26 16:06:49 -0300

    feat: criação de exceptions

---
+++ manga_be/src/main/java/com/reader/manga/exception/CreationErrorException.java
@@ -0,0 +1,8 @@
+package com.reader.manga.exception;
+
+public class CreationErrorException extends RuntimeException {
+
+    public CreationErrorException(String message) {
+        super(message);
+    }
+}
