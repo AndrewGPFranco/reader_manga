9725be0199f5ca930984fa8cc6734a29ec35f0a3
AndrewGPFranco <andrewgomes1328@gmail.com>
2024-11-15 16:31:53 -0300

    adicionando o spring security a aplicação

--- manga_be/src/main/java/com/reader/manga/service/ChapterService.java
+++ manga_be/src/main/java/com/reader/manga/service/ChapterService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @Service
 public class ChapterService {
@@ -67,7 +68,8 @@ public class ChapterService {
     }
 
     public Chapter getChapterByID(Long id) {
-        return repository.findById(id).orElseThrow(() -> new NotFoundException("Chapter not found."));
+        Supplier<NotFoundException> exceptionSupplier = () -> new NotFoundException("Chapter not found");
+        return repository.findById(id).orElseThrow(exceptionSupplier);
     }
 
 }
