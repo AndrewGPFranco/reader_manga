c68ba0ee9691495285a88ae1af6963acfd433aaf
AndrewGPFranco <andrewgomes1328@gmail.com>
2024-11-15 21:35:26 -0300

    ajustando detalhe de segurança de csrf

--- manga_be/src/main/java/com/reader/manga/config/SecurityConfig.java
+++ manga_be/src/main/java/com/reader/manga/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -18,7 +19,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((auth) -> auth
                 .anyRequest().authenticated()
         ).httpBasic(Customizer.withDefaults());
         return http.build();
