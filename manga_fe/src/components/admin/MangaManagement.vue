79d86208e49cf3b8a9b1f032edf085be3b016ee7
AndrewGPFranco <andrewgomes1328@gmail.com>
2024-11-20 01:30:11 -0300

    estrutura da tabela de mangás

--- manga_fe/src/components/admin/MangaManagement.vue
+++ manga_fe/src/components/admin/MangaManagement.vue
@@ -1,11 +1,72 @@
 <template>
-    <div>
-        
-    </div>
+    <n-table :single-line="false">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Status</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="manga in allManga" :key="manga.id">
+                <td>{{ manga.id }}</td>
+                <td>{{ manga.title }}</td>
+                <td>{{ manga.status }}</td>
+                <td class="tdButtons">
+                    <Delete class="buttonDelete" @click="deleteManga(manga.id)" />
+                    <Edit class="buttonEdit" @click="editManga(manga.id)" />
+                </td>
+            </tr>
+        </tbody>
+    </n-table>
 </template>
 
 <script setup lang="ts">
+import type MangaData from '@/interface/Manga';
+import { useMangaStore } from '@/store/MangaStore';
+import { onMounted, ref } from 'vue';
+import { TrashOutline as Delete, CreateOutline as Edit } from '@vicons/ionicons5'
 
+const mangaStore = useMangaStore();
+const allManga = ref<MangaData[]>([]);
+
+const deleteManga = (id: number) => {
+    // TODO implementar método
+}
+
+const editManga = (id: number) => {
+    // TODO implementar método
+}
+
+onMounted(async () => {
+    const newLocal = await mangaStore.getAllManga();
+    allManga.value = newLocal;
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.actions {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    text-align: center;
+}
+
+.tdButtons {
+    display: flex;
+    justify-content: space-around;
+}
+
+.tdButtons svg {
+    width: 25px;
+    height: 25px;
+}
+
+.buttonDelete, .buttonEdit { cursor: pointer; }
+
+.buttonDelete { color: rgba(255, 0, 0, 0.678); }
+.buttonEdit { color: rgb(0, 109, 0); }
+
+</style>
