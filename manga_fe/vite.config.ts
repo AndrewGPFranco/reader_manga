0bfb7af40cc0a846e2fd8133cafa431c6d1e25a1
AndrewGPFranco <andrewgomes1328@gmail.com>
2024-12-07 03:11:50 +0000

    arrumando pipeline quebrada

--- manga_fe/vite.config.ts
+++ manga_fe/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
