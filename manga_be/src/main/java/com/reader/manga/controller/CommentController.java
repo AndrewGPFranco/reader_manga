4f3e5ba290c9bcb639638b7978d91c63acbf7810
andrewgo <andrew.silva@duxus.com.br>
2025-01-11 01:16:30 -0300

    finalizando as operações importantes referente aos comentários em mangás.

--- manga_be/src/main/java/com/reader/manga/controller/CommentController.java
+++ manga_be/src/main/java/com/reader/manga/controller/CommentController.java
@@ -4,10 +4,9 @@ import com.reader.manga.dto.comment.CommentDTO;
 import com.reader.manga.service.CommentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/comment")
@@ -24,5 +23,44 @@ public class CommentController {
     public ResponseEntity<CommentDTO> registerCommentForManga(@RequestBody CommentDTO dto) {
         CommentDTO comment = commentService.registerComment(dto);
         return ResponseEntity.ok().body(comment);
+    }
+
+    @GetMapping("/manga/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<CommentDTO>> getCommentsByManga(@PathVariable Long id) {
+        List<CommentDTO> comments = commentService.getComments(id);
+        return ResponseEntity.ok().body(comments);
+    }
+
+    @GetMapping("/manga")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<CommentDTO>> getCommentsByManga() {
+        List<CommentDTO> comments = commentService.getAllComments();
+        return ResponseEntity.ok().body(comments);
+    }
+
+    @DeleteMapping("/all-manga")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteAllComments() {
+        commentService.deleteAllComments();
+    }
+
+    @DeleteMapping("/delete/all-manga/{idManga}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteAllCommentsById(@PathVariable Long idManga) {
+        commentService.deleteAllCommentsById(idManga);
+    }
+
+    @DeleteMapping("/delete/{idManga}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteCommentById(@PathVariable Long idManga) {
+        commentService.deleteCommentById(idManga);
+    }
+
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<CommentDTO> updateComment(@PathVariable Long id, @RequestBody CommentDTO comment) {
+        CommentDTO commentDto = commentService.updateComment(id, comment.commentText());
+        return ResponseEntity.ok().body(commentDto);
     }
 }
--- manga_be/src/main/java/com/reader/manga/service/CommentService.java
+++ manga_be/src/main/java/com/reader/manga/service/CommentService.java
@@ -6,16 +6,23 @@ import com.reader.manga.mapper.CommentMapper;
 import com.reader.manga.model.Comment;
 import com.reader.manga.model.Manga;
 import com.reader.manga.repository.CommentRepository;
+import com.reader.manga.repository.MangaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final MangaRepository mangaRepository;
     private final MangaService mangaService;
 
-    public CommentService(CommentRepository commentRepository, MangaService mangaService) {
+    public CommentService(CommentRepository commentRepository, MangaRepository mangaRepository, MangaService mangaService) {
         this.commentRepository = commentRepository;
+        this.mangaRepository = mangaRepository;
         this.mangaService = mangaService;
     }
 
@@ -30,4 +37,79 @@ public class CommentService {
         }
     }
 
+    /**
+     * Responsável por trazer todos os comentários de um mangá específico.
+     * @param idManga do mangá a ser resgatados os comentários.
+     * @return lista de comentários do mangá pesquisado.
+     */
+    public List<CommentDTO> getComments(Long idManga) {
+        try {
+            Manga mangaById = mangaService.findById(idManga);
+            List<CommentDTO> listCommentDto = new ArrayList<>();
+            mangaById.getComments().forEach(comment -> listCommentDto.add(CommentMapper.entityToDto(comment)));
+            return listCommentDto;
+        } catch (NotFoundException e) {
+            throw new NotFoundException(e.getMessage());
+        }
+    }
+
+    /**
+     * Responsável por buscar todos os comentários do sistema.
+     * @return todos os comentários do sistema.
+     */
+    public List<CommentDTO> getAllComments() {
+        List<CommentDTO> listCommentDto = new ArrayList<>();
+        List<Comment> allComments = commentRepository.findAll();
+
+        if(!allComments.isEmpty())
+            allComments.forEach(comment -> listCommentDto.add(CommentMapper.entityToDto(comment)));
+
+        return listCommentDto;
+    }
+
+    /**
+     * Responsável por remover todos os comentários do sistema.
+     */
+    public void deleteAllComments() {
+        List<Comment> allComments = commentRepository.findAll();
+        if(!allComments.isEmpty())
+            allComments.forEach(comment -> commentRepository.deleteById(comment.getId()));
+    }
+
+    /**
+     * Responsável por excluir todos os comentários de um mangá
+     * @param idManga do mangá a ter os comentários excluídos.
+     */
+    public void deleteAllCommentsById(Long idManga) {
+        try {
+            Manga mangaById = mangaService.findById(idManga);
+            mangaById.getComments().removeAll(mangaById.getComments());
+            mangaRepository.save(mangaById);
+        } catch (NotFoundException e) {
+            throw new NotFoundException(e.getMessage());
+        }
+    }
+
+    /**
+     * Deletar um comentário pelo id.
+     * @param id do comentário a ser excluído.
+     */
+    public void deleteCommentById(Long id) {
+        Optional<Comment> commentById = commentRepository.findById(id);
+        if(commentById.isPresent())
+            commentRepository.deleteById(id);
+        else
+            throw new NotFoundException("Nenhum comentário encontrado com o id: " + id);
+    }
+
+    public CommentDTO updateComment(Long id, String commentUser) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Nenhum comentário encontrado com o id: " + id));
+
+        UtilsService.updateField(commentUser, comment::setCommentText);
+
+        commentRepository.save(comment);
+
+        return CommentMapper.entityToDto(comment);
+    }
 }
