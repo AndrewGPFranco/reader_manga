a16673c54355cd28f1bf4b8eada37b2a4a0cf2eb
andrewgo <andrew.silva@duxus.com.br>
2025-01-29 22:26:17 -0300

    iniciando testes do service de capítulos

---
+++ manga_be/src/test/java/com/reader/manga/service/ChapterServiceTest.java
@@ -0,0 +1,100 @@
+package com.reader.manga.service;
+
+import com.reader.manga.dto.chapter.ChapterDTO;
+import com.reader.manga.enums.StatusType;
+import com.reader.manga.exception.CreationErrorException;
+import com.reader.manga.model.Chapter;
+import com.reader.manga.model.Manga;
+import com.reader.manga.repository.ChapterRepository;
+import com.reader.manga.repository.MangaRepository;
+import com.reader.manga.repository.PageRepository;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+class ChapterServiceTest {
+
+    @Spy
+    @InjectMocks
+    private ChapterService spyService;
+
+    @Mock
+    private ChapterRepository chapterRepository;
+
+    @Mock
+    private MangaRepository mangaRepository;
+
+    @Mock
+    private PageRepository pageRepository;
+
+    private static AutoCloseable closeable;
+
+    @BeforeEach
+    void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterAll
+    static void tearDown() throws Exception {
+        if (closeable != null) {
+            closeable.close();
+        }
+    }
+
+    private ChapterDTO getChapterDto() {
+        return ChapterDTO.builder()
+                .title("Capítulo 1")
+                .description("Primeiro capítulo de One Piece.")
+                .mangaId(1L)
+                .build();
+    }
+
+    private Manga getEntityManga() {
+        return Manga.builder()
+                .id(1L)
+                .size(1135)
+                .gender("Shounen")
+                .title("One Piece")
+                .closingDate(null)
+                .chapters(List.of())
+                .comments(List.of())
+                .author("Eiichiro Oda")
+                .creationDate(new Date())
+                .status(StatusType.ONGOING)
+                .description("Mangá sobre piratas.")
+                .image("www.onepiece.com.br/imagemdecapa.png")
+                .build();
+    }
+
+    @Test
+    @DisplayName("Deve criar um capítulo com sucesso")
+    void testeCreateChapter() {
+        // Arrange
+        Manga manga = getEntityManga();
+        Mockito.when(mangaRepository.findById(1L)).thenReturn(Optional.of(manga));
+
+        // Act
+        Assertions.assertDoesNotThrow(() -> spyService.createChapter(getChapterDto()));
+
+        // Assert
+        Mockito.verify(chapterRepository, Mockito.times(1)).save(Mockito.any(Chapter.class));
+    }
+
+    @Test
+    @DisplayName("Lança uma exception por não encontrar nenhum mangá com o id informado")
+    void testeCreateChapterComErro() {
+        // Act & Assert
+        CreationErrorException exception = Assertions
+                .assertThrows(CreationErrorException.class, () -> spyService.createChapter(getChapterDto()));
+
+        // Assert
+        String expected = "Erro ao salvar capítulo. Por favor, tente novamente...";
+        String result = exception.getMessage();
+
+        Assertions.assertTrue(result.contains(expected));
+    }
+
+}
