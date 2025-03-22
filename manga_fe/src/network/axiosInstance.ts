75027cbf4b053072f80fab327d1f75569da56a09
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-03-22 03:10:05 -0300

    voltando endereço da api

--- manga_fe/src/network/axiosInstance.ts
+++ manga_fe/src/network/axiosInstance.ts
@@ -1,6 +1,5 @@
 import axios, { type AxiosInstance } from 'axios'
 
 export const api: AxiosInstance = axios.create({
-    // baseURL: "http://localhost:8080"
-    baseURL: "https://6367-2804-7f0-b240-b768-307f-125f-4d76-27ad.ngrok-free.app"
+    baseURL: "http://localhost:8080"
 });
