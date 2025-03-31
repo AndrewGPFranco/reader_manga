9467926fcb6a5f0064c456575a0dfc7f5ff07674
AndrewGPFranco <andrewgomes1328@gmail.com>
2025-03-31 18:12:54 -0300

    ajustando método que obtém progresso para mangás que não foram adicionados na lista do usuário

--- manga_be/src/main/java/com/reader/manga/domain/services/UserChapterService.java
+++ manga_be/src/main/java/com/reader/manga/domain/services/UserChapterService.java
@@ -5,7 +5,6 @@ import com.reader.manga.domain.entities.mangas.Chapter;
 import com.reader.manga.domain.entities.mangas.Manga;
 import com.reader.manga.domain.entities.users.User;
 import com.reader.manga.domain.entities.users.UserChapter;
-import com.reader.manga.domain.entities.users.UserManga;
 import com.reader.manga.domain.enums.StatusType;
 import com.reader.manga.ports.repositories.ChapterRepository;
 import com.reader.manga.ports.repositories.UserChapterRepository;
@@ -22,7 +21,6 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserChapterService {
 
-    private final MangaService mangaService;
     private final UserMangaService userService;
     private final UserRepository userRepository;
     private final ChapterRepository chapterRepository;
@@ -69,27 +67,16 @@ public class UserChapterService {
         User user = userService.getUserById(idUser);
         Map<Long, Integer> progressos = new HashMap<>();
 
-        boolean isPossui = verificaUsuarioPossuiManga(user, mangaRecuperado);
-
-        if(isPossui) {
-            // Mangás do usuário
-            List<UserManga> userMangas = user.getUserMangas();
-            for (UserManga mangas : userMangas) {
-                Manga manga = mangas.getManga();
-                for (Chapter chapter : manga.getChapters()) {
-                    UserChapter chapterId = userChapterRepository.findByIdChapterAndUser(chapter.getId(), user.getId());
-                    Integer progressoAtual = chapterId != null ? chapterId.getProgress() : 0;
-                    progressos.put(chapter.getId(), progressoAtual);
-                }
-            }
-        }
-
+        adicionaCapituloAoMapaProgresso(mangaRecuperado, user, progressos);
         return progressos;
     }
 
-    private boolean verificaUsuarioPossuiManga(User user, Manga mangaRecuperado) {
-        return user.getUserMangas().stream().anyMatch(
-                m -> m.getManga().getId().equals(mangaRecuperado.getId()));
+    private void adicionaCapituloAoMapaProgresso(Manga manga, User user, Map<Long, Integer> progressos) {
+        for (Chapter chapter : manga.getChapters()) {
+            UserChapter chapterId = userChapterRepository.findByIdChapterAndUser(chapter.getId(), user.getId());
+            Integer progressoAtual = chapterId != null ? chapterId.getProgress() : 0;
+            progressos.put(chapter.getId(), progressoAtual);
+        }
     }
 
     public void atualizaProgresso(List<Chapter> chapters, Map<Long, Integer> mapaProgressos) {
