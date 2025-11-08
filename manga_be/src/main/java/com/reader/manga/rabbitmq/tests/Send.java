b269dbea3278bd6d5d7a1e01f905bb473fc94297
andrewgo <andrew.silva@duxus.com.br>
2025-11-08 01:43:13 -0300

    feat: hello world com rabbitmq

---
+++ manga_be/src/main/java/com/reader/manga/rabbitmq/tests/Send.java
@@ -0,0 +1,28 @@
+package com.reader.manga.rabbitmq.tests;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("andrew");
+        factory.setPassword("reader");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+
+}
